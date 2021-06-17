2be72c150710259e112b6daeddecffcb34ce9c66
Michael Martinez <michael.martinez@sky.uk>
2021-06-17 08:57:31 +0100

    completed styles

--- src/components/lib.exercise.js
+++ src/components/lib.exercise.js
@@ -1,5 +1,9 @@
-import styled from '@emotion/styled'
+import styled from '@emotion/styled/macro'
 import {Dialog as ReachDialog} from '@reach/dialog'
+import * as mq from 'styles/media-queries'
+import * as colors from 'styles/colors'
+import { FaSpinner } from 'react-icons/fa';
+import { keyframes } from '@emotion/core';
 
 // 🐨 create a button styled component here called "Button"
 // make it look nice and allow it to support a "variant" prop which can be
@@ -12,6 +16,28 @@ import {Dialog as ReachDialog} from '@reach/dialog'
 // do in the final example. Just make sure you understand how to create the
 // styled component and accept a prop to change which styles apply.
 
+const primaryBtn = {
+  background: colors.indigo,
+  color: colors.base,
+};
+const secondaryBtn = {
+  background: colors.gray,
+  color: colors.text,
+};
+const buttonStyles = {
+  primary: primaryBtn,
+  secondary: secondaryBtn,
+};
+
+const Button = styled.button(
+  {
+    padding: '10px 15px',
+    border: '0',
+    lineHeight: '1',
+    borderRadius: '3px',
+  },
+  ({ variant }) => buttonStyles[variant]
+);
 // 🐨 Feel free to create as many reusable styled components here as you'd like
 // 💰 in my finished version I have: Button, Input, CircleButton, Dialog, FormGroup
 
@@ -35,10 +61,20 @@ import {Dialog as ReachDialog} from '@reach/dialog'
 //   border: '1px solid #f1f1f4',
 //   background: '#f1f2f7',
 //   padding: '8px 12px',
+const Input = styled.input`
+  border-radius: 3px;
+  border: 1px solid ${colors.gray10};
+  background: ${colors.gray};
+  padding: 8px 12px;
+`;
 
 // FormGroup
 //   display: 'flex',
 //   flexDirection: 'column',
+const FormGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 // 💰 I'm giving a few of these to you:
 const CircleButton = styled.button({
@@ -50,9 +86,9 @@ const CircleButton = styled.button({
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
-  background: 'white',
-  color: '#434449',
-  border: `1px solid #f1f1f4`,
+  background: colors.base,
+  color: colors.text,
+  border: `1px solid ${colors.gray10}`,
   cursor: 'pointer',
 })
 
@@ -62,10 +98,20 @@ const Dialog = styled(ReachDialog)({
   paddingBottom: '3.5em',
   boxShadow: '0 10px 30px -5px rgba(0, 0, 0, 0.2)',
   margin: '20vh auto',
-  '@media (max-width: 991px)': {
+  [mq.small]: {
     width: '100%',
     margin: '10vh auto',
   },
 })
 
-export {CircleButton, Dialog}
+const spin = keyframes`
+  from {transform:rotate(0deg);}
+  to {transform:rotate(360deg);}
+`;
+
+const Spinner = styled(FaSpinner)({
+  animation: `${spin} 1s linear infinite`,
+  marginLeft: `5px`,
+});
+
+export {CircleButton, Dialog, Button, Input, FormGroup, Spinner}
